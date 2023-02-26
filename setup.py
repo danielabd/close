7b3c4bfa54d51bd77f9c4bbaaec3e3079d6aae5a
Daniela Ben-David <bendavid.daniela@gmail.com>
2023-02-26 23:05:11 +0200

    add setupfile

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="close",  # Replace with your own username
+    version="",
+    author="",
+    author_email="",
+    description="",
+    #long_description=None,
+    #long_description_content_type="text/markdown",
+    #url="https://github.com/avrech/learning2cut",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    #    "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+    ],
+    python_requires='>=3.6',
+)
